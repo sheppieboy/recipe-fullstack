2e9443f279e256e9e94023770d5bfa595fa826c5
Luke Sheppard <sheppiegit@gmail.com>
2022-11-04 19:28:31 -0400

    Added static sidebar items, this consists of MUI list with item buttons that are clickable.  This is the basic visual outline of the sidebar without any responsiveness.

--- client/src/components/Sidebar.tsx
+++ client/src/components/Sidebar.tsx
@@ -1,13 +1,70 @@
+import {
+  AccountBox,
+  Group,
+  Home,
+  ModeNight,
+  Settings,
+} from "@mui/icons-material";
+import {
+  Box,
+  List,
+  ListItem,
+  ListItemButton,
+  ListItemIcon,
+  ListItemText,
+  Switch,
+  Typography,
+} from "@mui/material";
 
-import { Box, Typography } from "@mui/material"
-
-
-export const Sidebar = () =>{
-    return (
-        <Box sx={{bgcolor: "yellow"}}>
-            <Typography>
-                Sidebar 
-            </Typography>
-        </Box>
-    )
-}
+export const Sidebar = () => {
+  return (
+    <Box sx={{ flex: 1, p: 2, bgcolor: "yellow" }}>
+      <Typography>Sidebar</Typography>
+      <List>
+        {/*Homepage */}
+        <ListItem disablePadding>
+          <ListItemButton component="a" href="">
+            <ListItemIcon>
+              <Home />
+            </ListItemIcon>
+            <ListItemText primary="Homepage" />
+          </ListItemButton>
+        </ListItem>
+        {/* Profile */}
+        <ListItem disablePadding>
+          <ListItemButton component="a" href="">
+            <ListItemIcon>
+              <AccountBox />
+            </ListItemIcon>
+            <ListItemText primary="Profile" />
+          </ListItemButton>
+        </ListItem>
+        {/* All Users page */}
+        <ListItem disablePadding>
+          <ListItemButton component="a" href="">
+            <ListItemIcon>
+              <Group />
+            </ListItemIcon>
+            <ListItemText primary="Users" />
+          </ListItemButton>
+        </ListItem>
+        {/* Settings */}
+        <ListItem disablePadding>
+          <ListItemButton component="a" href="">
+            <ListItemIcon>
+              <Settings />
+            </ListItemIcon>
+            <ListItemText primary="Settings" />
+          </ListItemButton>
+        </ListItem>
+        {/* Dark Mode*/}
+        <ListItem>
+          <ListItemIcon>
+            <ModeNight />
+          </ListItemIcon>
+          <Switch />
+        </ListItem>
+      </List>
+    </Box>
+  );
+};
