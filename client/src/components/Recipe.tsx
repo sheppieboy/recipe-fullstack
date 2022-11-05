c535a47dff7c7ef4fda1a2dc149031e4e8324fa5
Luke Sheppard <sheppiegit@gmail.com>
2022-11-04 21:09:05 -0400

    Added a Recipe component that displays a recipe in the Feed Component. It will display a Card that has the username, date, recipe description, recipe image, and several buttons such as go to full recipe, like recipe, and add recipe to favorouites.

---
+++ client/src/components/Recipe.tsx
@@ -0,0 +1,96 @@
+import {
+  AddCircle,
+  ArrowCircleRight,
+  Favorite,
+  FavoriteBorder,
+} from "@mui/icons-material";
+import {
+  Avatar,
+  Card,
+  CardActions,
+  CardContent,
+  CardHeader,
+  CardMedia,
+  Checkbox,
+  createTheme,
+  IconButton,
+  ThemeProvider,
+  Tooltip,
+  Typography,
+} from "@mui/material";
+
+import { FakeRecipe } from "./types";
+
+type Props = {
+  recipe: FakeRecipe;
+};
+
+function randomColor() {
+  let hex = Math.floor(Math.random() * 0xffffff);
+  let color = "#" + hex.toString(16);
+  return color;
+}
+
+function getFirst2Letters(username: string) {
+  return username.slice(0, 2);
+}
+
+const themeForToolTip = createTheme({
+  components: {
+    MuiTooltip: {
+      styleOverrides: {
+        tooltip: {
+          fontWeight: "light",
+          color: "white",
+          backgroundColor: "black",
+        },
+      },
+    },
+  },
+});
+
+export const Recipe = ({
+  recipe: { username, image, date, description, title },
+}: Props) => {
+  return (
+    <Card sx={{ m: 2 }}>
+      <CardHeader
+        avatar={
+          <Avatar sx={{ bgcolor: randomColor() }}>
+            {getFirst2Letters(username)}
+          </Avatar>
+        }
+        action={
+          <ThemeProvider theme={themeForToolTip}>
+            <Tooltip title="See full recipe">
+              <IconButton aria-label="go to recipe">
+                <ArrowCircleRight />
+              </IconButton>
+            </Tooltip>
+          </ThemeProvider>
+        }
+        title={title}
+        subheader={date}
+      />
+      <CardMedia component="img" height="20%" image={image} alt={title} />
+      <CardContent>
+        <Typography>{description}</Typography>
+      </CardContent>
+      <CardActions disableSpacing>
+        <IconButton aria-label="add to favorites">
+          <Checkbox
+            icon={<FavoriteBorder />}
+            checkedIcon={<Favorite sx={{ color: "red" }} />}
+          />
+        </IconButton>
+        <ThemeProvider theme={themeForToolTip}>
+          <Tooltip title="Add to Favourites">
+            <IconButton aria-label="add to favourites">
+              <AddCircle sx={{ color: randomColor() }} />
+            </IconButton>
+          </Tooltip>
+        </ThemeProvider>
+      </CardActions>
+    </Card>
+  );
+};
---
+++ client/src/components/types.tsx
@@ -0,0 +1,7 @@
+export type FakeRecipe = {
+  title: string;
+  username: string;
+  date: string;
+  image: string;
+  description: string;
+};
