55c5eb28681bf8096bae5b7309dc26b2d5fe2ccd
Luke Sheppard <sheppiegit@gmail.com>
2022-11-04 22:38:12 -0400

    Added a RecentImageList component for the Right bar component.  The RecentImageList is composed of the most recent recipes uploaded, it displays the recipes images in a grid system and the username of the user who posted it as well as the title of the image. The component was filled with fake data from the fakeData file.

---
+++ client/src/components/RecentImageList.tsx
@@ -0,0 +1,40 @@
+import { ArrowCircleRight } from "@mui/icons-material";
+import {
+  IconButton,
+  ImageList,
+  ImageListItem,
+  ImageListItemBar,
+  Tooltip,
+} from "@mui/material";
+import { fakeData } from "./fakeData";
+
+export const RecentImageList = () => {
+  return (
+    <ImageList>
+      {fakeData.map((recipe) => (
+        <ImageListItem key={recipe.image}>
+          <img
+            src={`${recipe.image}?w=248&fit=crop&auto=format`}
+            srcSet={`${recipe.image}?w=248&fit=crop&auto=format&dpr=2 2x`}
+            alt={recipe.title}
+            loading="lazy"
+          />
+          <ImageListItemBar
+            title={recipe.title}
+            subtitle={`@${recipe.username}`}
+            actionIcon={
+              <Tooltip title="check recipe out">
+                <IconButton
+                  sx={{ color: "rgba(255, 255, 255, 0.54)" }}
+                  aria-label={`info about ${recipe.title}`}
+                >
+                  <ArrowCircleRight />
+                </IconButton>
+              </Tooltip>
+            }
+          />
+        </ImageListItem>
+      ))}
+    </ImageList>
+  );
+};
