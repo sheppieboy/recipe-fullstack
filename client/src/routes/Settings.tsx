4bdca50701cc36ed693d02a0e0547af7055ce320
Luke Sheppard <sheppiegit@gmail.com>
2022-11-07 18:06:53 -0500

    Added in React Router and created basic pages for each category: Home, Users, Favorites, Profile, Settings. These will be the different pages of the app, they currently only display the text stating their name i.e., Favorities display <Box> Favorites <Box>

---
+++ client/src/routes/Settings.tsx
@@ -0,0 +1,10 @@
+import { Typography } from "@mui/material";
+import { Box } from "@mui/system";
+
+export const Settings = () => {
+  return (
+    <Box>
+      <Typography>Settings</Typography>
+    </Box>
+  );
+};
---
+++ client/src/routes/Users.tsx
@@ -0,0 +1,10 @@
+import { Typography } from "@mui/material";
+import { Box } from "@mui/system";
+
+export const Users = () => {
+  return (
+    <Box>
+      <Typography>Users</Typography>
+    </Box>
+  );
+};
