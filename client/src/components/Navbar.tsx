c0add84ed015aed49667ba9633d4e348feb93b7e
Luke Sheppard <sheppiegit@gmail.com>
2022-11-07 21:15:08 -0500

    Added UI and styling to Navbar along with a clickable menu, user avatar, and search bar. As well, added routing to the menu except for the logout menu item.

--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -1,25 +1,62 @@
-
-import { AppBar, Toolbar, Typography } from "@mui/material"
-import {styled} from "@mui/material/styles";
-
+import {
+  AppBar,
+  Toolbar,
+  Typography,
+  Box,
+  Avatar,
+  InputBase,
+  Menu,
+  MenuItem,
+} from "@mui/material";
+import { styled } from "@mui/material/styles";
+import { useState } from "react";
+import { Link } from "react-router-dom";
 
 const StyledToolbar = styled(Toolbar)({
-    display: "flex",
-    justifyContent: "space-between",
+  display: "flex",
+  justifyContent: "space-between",
 }) as typeof Toolbar;
 
-export const Navbar = () =>{
-    return (<AppBar>
-        <StyledToolbar>
-            <Typography>
-                Logo
-            </Typography>
-            <Typography>
-                Search bar
-            </Typography>
-            <Typography>
-                profile pic + tooltip
-            </Typography>
-        </StyledToolbar>
-    </AppBar>)
-}
+const Search = styled(Box)(({ theme }) => ({
+  backgroundColor: "white",
+  padding: "0 10px",
+  borderRadius: theme.shape.borderRadius,
+  width: "40%",
+}));
+
+export const Navbar = () => {
+  const [open, setOpen] = useState(false);
+  return (
+    <AppBar>
+      <StyledToolbar>
+        <Typography>Logo</Typography>
+        <Search>
+          <InputBase placeholder="search" />
+        </Search>
+        <Box sx={{ display: "flex", alignItems: "center" }}>
+          <Avatar sx={{ width: 50, height: 50 }} onClick={() => setOpen(true)}>
+            username
+          </Avatar>
+        </Box>
+      </StyledToolbar>
+      <Menu
+        id="positioned-menu"
+        aria-labelledby="user-button"
+        open={open}
+        onClose={() => setOpen(false)}
+        anchorOrigin={{
+          vertical: "top",
+          horizontal: "right",
+        }}
+      >
+        <MenuItem component={Link} to={"profile"}>
+          Profile
+        </MenuItem>
+        <MenuItem component={Link} to={"settings"}>
+          Settings
+        </MenuItem>
+        <MenuItem>Logout</MenuItem>
+      </Menu>
+    </AppBar>
+  );
+};
