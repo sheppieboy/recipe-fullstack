9bc6ba0d0717f8f4314bdade4408b1a8034e774d
Luke Sheppard <sheppiegit@gmail.com>
2022-11-08 21:43:00 -0500

    Added a UserCard component for the users page.  It is clickable and in the future will direct to the specific user's profile, but does nothing on click currently.

---
+++ client/src/components/UserCard.tsx
@@ -0,0 +1,69 @@
+import { Comment, Fastfood, Favorite } from "@mui/icons-material";
+import {
+  Avatar,
+  Box,
+  Card,
+  CardActionArea,
+  CardContent,
+  CardHeader,
+  Typography,
+} from "@mui/material";
+
+export const UserCard = () => {
+  const color = "#E89A96";
+  const bgColor = "#D6FFFF";
+
+  return (
+    <Box sx={{ width: 300, p: 1 }}>
+      <Card sx={{ bgcolor: bgColor }}>
+        <CardActionArea>
+          <CardHeader
+            avatar={<Avatar>username</Avatar>}
+            title={"username"}
+            subheader={"date"}
+          />
+          <CardContent
+            sx={{
+              display: "flex",
+              alignItems: "center",
+              justifyContent: "space-between",
+            }}
+          >
+            <Box
+              sx={{
+                display: "flex",
+                alignItems: "center",
+                gap: 1,
+                color: color,
+              }}
+            >
+              <Typography>42</Typography>
+              <Favorite />
+            </Box>
+            <Box
+              sx={{
+                display: "flex",
+                alignItems: "center",
+                gap: 1,
+                color: color,
+              }}
+            >
+              <Typography>46</Typography>
+              <Comment />
+            </Box>
+            <Box
+              sx={{
+                display: "flex",
+                alignItems: "center",
+                gap: 1,
+              }}
+            >
+              <Typography>5</Typography>
+              <Fastfood />
+            </Box>
+          </CardContent>
+        </CardActionArea>
+      </Card>
+    </Box>
+  );
+};
