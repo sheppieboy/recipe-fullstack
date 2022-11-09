3a3b6acc3b19aeb948d1892a5696e8ef3aae1b84
Luke Sheppard <sheppiegit@gmail.com>
2022-11-08 21:51:57 -0500

    Added the UserCard to the Users page.

--- client/src/routes/Users.tsx
+++ client/src/routes/Users.tsx
@@ -1,9 +1,17 @@
 import { Typography, Box } from "@mui/material";
+import { Stack } from "@mui/system";
+import { UserCard } from "../components/UserCard";
 
 export const Users = () => {
   return (
     <Box sx={{ pt: 9 }}>
       <Typography>Users</Typography>
+      <Stack>
+        <UserCard />
+        <UserCard />
+        <UserCard />
+        <UserCard />
+      </Stack>
     </Box>
   );
 };
